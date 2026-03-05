b12e3f495a36bd38e5c3ce771114ac8a9b243812
DESKTOP-4LFM7HV\jett <hydrapops@gmail.com>
2026-03-05 18:11:36 +0800

    fix: New Expenses Not Appearing at Top of List

--- backend/app/models/expense.rb
+++ backend/app/models/expense.rb
@@ -1,3 +1,10 @@
 class Expense < ApplicationRecord
   belongs_to :category
+  before_create :set_default_payer_name
+  
+  private
+  
+  def set_default_payer_name
+    self.payer_name ||= 'User'
+  end
 end
